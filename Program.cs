77bc57057dc47c2cd7bf0f7a145808026f29dfd4
tanvi-khade <tanvi@astrumlabs.co>
2021-04-30 01:21:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*  
+ *  Hello World!
+ *  I am using this project as a way of keeping track of my solutions to LeetCode Problems (yes I am hoarder ;))
+ *  My plan is to have one file per problem but might as well use folders if the situation demands.
+ *  If you would like test any of the Problems Edit the Main() below as the question demands, hopefully I'll be able write 
+ *  enough clean code for you to understand how to do that.  
+ *  Cheers!
+*/
+
+using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
